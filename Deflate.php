902f84512a5231333a46f8b3ce9c484266ee91dc
ProcessID <34098270+ProcessID@users.noreply.github.com>
2020-07-07 15:23:11 +0200

    Add files via upload

---
+++ Deflate.php
@@ -0,0 +1,126 @@
+<?php
+   // Compression d'un fichier ou d'une chaine en DEFLATE
+   // -------------------
+   // -- Instanciation --
+   // -------------------
+   // $obj = new Bzip2($inputType, $inputData, $outputType, $outputFile);
+   // $inputType : FILE | VARIABLE
+   // $inputData : si $inputType = FILE : $inputData contient le chemin complet du fichier à compresser. si $inputType = VARIABLE : $inputData contient la chaîne à compresser
+   // $outputType : FILE | VARIABLE
+   // $outputFile : si $outputType = FILE : $outputFile contient le chemin complet du fichier de sortie
+   
+   namespace ProcessID\Compress;
+   
+   class Deflate {
+      protected $_inputType;
+      protected $_inputData;
+      protected $_inputFilePointer;
+      protected $_outputType;
+      protected $_outputFile;
+      protected $_outputFilePointer;
+      protected $_outputData;
+      protected $_compressionLevel;
+      
+      // Constantes de $_inputType et $_outputType
+      const FILE = 1;
+      const VARIABLE = 2;
+      
+      function __construct($inputType, $inputData, $outputType, $outputFile = '') {
+         $this->inputData = $inputData;
+         $this->setInputType($inputType);
+         $this->outputFile = $outputFile;
+         $this->setOutputType($outputType);
+         $this->outputData = '';
+         $this->setCompressionLevel(4);
+      }
+      
+      function setCompressionLevel($compression_level) {
+         if (is_int($compression_level)) {
+            if ($compression_level < 1) {
+               $compression_level = 1;
+            }
+            if ($compression_level > 9) {
+               $compression_level = 9;
+            }
+            $this->compressionLevel = $compression_level;
+         } else {
+            trigger_error('Le niveau de compression doit etre un entier entre 1 et 9 (9 est la meilleure compression)', E_USER_ERROR);
+         }
+      }
+      
+      function setInputType($inputType) {
+         if (is_int($inputType)) {
+            if ($inputType == self::FILE || $inputType == self::VARIABLE) {
+               $this->inputType = $inputType;
+               
+               if ($this->inputType == self::FILE) {
+                  if (!strlen($this->inputData)) {
+                     trigger_error('Vous devez preciser le fichier d\'entree', E_USER_ERROR);
+                  } else {
+                     $this->inputFilePointer = fopen($this->inputData,'rb');
+                     if (!$this->inputFilePointer) {
+                        trigger_error('Impossible d\'ouvrir le fichier d\'entree : ' . $this->inputData, E_USER_ERROR);
+                     }
+                  }
+               }
+            } else {
+               trigger_error('La methode d\'entree doit etre FILE ou VARIABLE', E_USER_ERROR);
+            }
+         } else {
+            trigger_error('La methode d\'entree doit etre FILE ou VARIABLE', E_USER_ERROR);
+         }
+      }
+      
+      function setOutputType($outputType) {
+         if (is_int($outputType)) {
+            if ($outputType == self::FILE || $inputType == self::VARIABLE) {
+               $this->outputType = $outputType;
+               
+               if ($this->outputType == self::FILE) {
+                  if (!strlen($this->outputFile)) {
+                     trigger_error('Vous devez preciser le fichier de sortie avec la methode de sortie FILE', E_USER_ERROR);
+                  } else {
+                     $this->outputFilePointer = fopen($this->outputFile,'w');
+                     if (!$this->outputFilePointer) {
+                        trigger_error('Impossible d\'ecrire dans le fichier : ' . $this->outputFile, E_USER_ERROR);
+                     }
+                  }
+               }
+            } else {
+               trigger_error('La methode de sortie doit être FILE ou VARIABLE', E_USER_ERROR);
+            }
+         } else {
+            trigger_error('La methode de sortie doit être FILE ou VARIABLE', E_USER_ERROR);
+         }
+      }
+      
+      function compress() {
+         if ($this->inputType == self::FILE) {
+            stream_filter_append($this->inputFilePointer, 'zlib.deflate', STREAM_FILTER_READ, $this->compressionLevel);
+            while (!feof($this->inputFilePointer)) {
+               $str = fread($this->inputFilePointer, 8388608);
+               if ($this->outputType == self::FILE) {
+                  fwrite($this->outputFilePointer, $str);
+               } else {
+                  $this->outputData .= $str;
+               }
+            }
+            fclose ($this->inputFilePointer);
+         } else {
+            if ($this->outputType == self::FILE) {
+               fwrite($this->outputFilePointer, gzdeflate($this->inputData,$this->compressionLevel));
+            } else {
+               $this->outputData .= gzdeflate($this->inputData,$this->compressionLevel);
+            }
+         }
+         if ($this->outputType == self::FILE) {
+            fflush($this->outputFilePointer);
+            fclose($this->outputFilePointer);
+            return true;
+         } else {
+            return $this->outputData;
+         }
+      }
+      
+   }
+?>
